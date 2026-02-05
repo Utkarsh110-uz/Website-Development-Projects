93ea775f91dcac8c2bdea4ba06334ab708fecd61
Utkarsh110-uz <utkarsh66488@gmail.com>
2026-02-05 22:35:51 +0530

    Started work on spotify clone

---
+++ Netflix clone/script.js
@@ -0,0 +1,25 @@
+const movie1 = document.querySelector(".m1-poster");
+const movie2 = document.querySelector(".m2-poster");
+const movie3 = document.querySelector(".m3-poster");
+const movie4 = document.querySelector(".m4-poster");
+const movie5 = document.querySelector(".m5-poster");
+
+movie1.addEventListener("click", ()=>{
+    location.href = "https://youtu.be/fUiJulYbUaE?si=n1crG_HvQt7gsKVQ";
+});
+
+movie2.addEventListener("click", ()=>{
+    location.href = "https://youtu.be/l5f_sZPdC0c?si=qTf3u_mtcdyDJjaq";
+});
+
+movie3.addEventListener("click", ()=>{
+    location.href = "https://youtu.be/BBEr-Yh5CxQ?si=pHjmLRcGq1K4hlDs";
+});
+
+movie4.addEventListener("click", ()=>{
+    location.href = "https://youtu.be/eSgJ8PfSUSk?si=FCgUPcc16YWAEUQ_";
+});
+
+movie5.addEventListener("click", ()=>{
+    location.href = "https://youtu.be/jNuKwlKJx2E?si=WF6sXeoqWdYo1ilO";
+});
